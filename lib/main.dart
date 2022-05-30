63bc838131062731bc4a1bf856959af062a85dae
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2022-05-30 20:24:40 +0000

    Show the appropriate screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,73 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
+import 'package:mobx_reminders/dialogs/show_auth_error_dialog.dart';
+import 'package:mobx_reminders/loading/loading_screen.dart';
+import 'package:mobx_reminders/state/app_state.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'views/login_view.dart';
+import 'views/register_view.dart';
+import 'views/reminders_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  runApp(
+    Provider<AppState>(
+      create: (_) => AppState()..initialize(),
+      child: const App(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: ReactionBuilder(
+        builder: (context) {
+          return autorun(
+            (_) {
+              // handle loading screen
+              final isLoading = context.read<AppState>().isLoading;
+              if (isLoading) {
+                LoadingScreen.instance().show(
+                  context: context,
+                  text: 'Loading...',
+                );
+              } else {
+                LoadingScreen.instance().hide();
+              }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+              // handle auth error
+              final authError = context.read<AppState>().authError;
+              if (authError != null) {
+                showAuthErrorDialog(context: context, authError: authError);
+              }
+            },
+          );
+        },
+        child: Observer(
+          builder: (context) {
+            switch (context.read<AppState>().currentScreen) {
+              case AppScreen.login:
+                return const LoginView();
+              case AppScreen.register:
+                return const RegisterView();
+              case AppScreen.reminders:
+                return const RemindersView();
+            }
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
